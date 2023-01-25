d3d60f9ba3cf1b61ad46edbc0117c0615d410f9e
gerges <gergess662@gmail.com>
2023-01-26 01:55:11 +0200

    2

--- Project_for_Girgess/Salary.Designer.cs
+++ Project_for_Girgess/Salary.Designer.cs
@@ -5,6 +5,12 @@
         /// <summary>
         /// Required designer variable.
         /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         
     }
 }
