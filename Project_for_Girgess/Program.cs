fa548aa9ee1021ed5335c57b954f75333b39107f
gerges <gergess662@gmail.com>
2023-01-26 01:57:28 +0200

    12

--- Project_for_Girgess/Program.cs
+++ Project_for_Girgess/Program.cs
@@ -8,6 +8,15 @@ namespace Project_for_Girgess
 {
     internal static class Program
     {
-       
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
     }
 }
