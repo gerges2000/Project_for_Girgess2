f781747f29d44bbe314f894dca4d0b5b966d052e
gerges <gergess662@gmail.com>
2023-01-26 01:58:28 +0200

    15

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -362,6 +362,25 @@
 
         #endregion
 
-        
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button AddBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button EditBtn;
+        private Guna.UI2.WinForms.Guna2DataGridView DeptList;
+        private System.Windows.Forms.TextBox DeptName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label EmpPage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button DeleteBtn;
     }
 }
