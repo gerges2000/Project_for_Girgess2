32c5730cdc1f697f3ef1b60d8f4705f80be515b5
gerges <gergess662@gmail.com>
2023-01-26 02:00:17 +0200

    22

--- Project_for_Girgess/Functions.cs
+++ Project_for_Girgess/Functions.cs
@@ -30,7 +30,17 @@ namespace Project_for_Girgess
             sda.Fill(dt);
             return dt;
         }
-        
+        public int SetData(string Query)
+        {
+            int cnt = 0;
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            cmd.CommandText = Query;
+            cnt=cmd.ExecuteNonQuery();
+            return cnt;
+        }
 
     }
 }
